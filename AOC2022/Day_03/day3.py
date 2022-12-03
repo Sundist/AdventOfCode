9c05d6c95b7386bf230a3baff98b97ade30e1a74
Sundist <sundisttt@gmail.com>
2022-12-03 16:16:22 +0300

    AOC2022

--- AOC2022/Day_03/day3.py
+++ AOC2022/Day_03/day3.py
@@ -1,14 +1,29 @@
-
-
 data = open('input.txt', 'r')
-sumOfPriorities = 0
-for rucksack in data.readlines():
-    rucksack = rucksack.strip('\n')
-    compartment_1 = rucksack[:(len(rucksack) // 2)]
-    compartment_2 = rucksack[(len(rucksack) // 2):]
-    priority = ''.join(set(compartment_1).intersection(compartment_2))
-    if priority.islower():
-        sumOfPriorities += (ord(priority) % 96)
-    elif priority.isupper():
-        sumOfPriorities += (ord(priority) - 38)
-print(sumOfPriorities)
+## PART 1 ##
+# sumOfPriorities = 0
+# for rucksack in data.readlines():
+#     rucksack = rucksack.strip('\n')
+#     compartment_1 = rucksack[:(len(rucksack) // 2)]
+#     compartment_2 = rucksack[(len(rucksack) // 2):]
+#     priority = ''.join(set(compartment_1).intersection(compartment_2))
+#     if priority.islower():
+#         sumOfPriorities += (ord(priority) % 96)
+#     elif priority.isupper():
+#         sumOfPriorities += (ord(priority) - 38)
+# print(sumOfPriorities)
+
+## PART 2 ##
+
+sumOfPrioritiesBadges = 0
+
+for elem in range(0, 300, 3):
+    line1 = data.readline().strip('\n')
+    line2 = data.readline().strip('\n')
+    line3 = data.readline().strip('\n')
+    badge = ''.join(set(line1).intersection(line2).intersection(line3))
+    if badge.islower():
+        sumOfPrioritiesBadges += (ord(badge) % 96)
+    elif badge.isupper():
+        sumOfPrioritiesBadges += (ord(badge) - 38)
+#    print(badge, ord(badge))
+print(sumOfPrioritiesBadges)
