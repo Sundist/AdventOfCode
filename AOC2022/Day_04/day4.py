15cf630199baf1a4fe8651dff3d7fde62dceed0f
Sundist <sundisttt@gmail.com>
2022-12-04 12:45:21 +0300

    AOC2022 day4

---
+++ AOC2022/Day_04/day4.py
@@ -0,0 +1,28 @@
+data = open('input.txt', 'r')
+firstElfList = list()
+secondElfList = list()
+fullyContainCounter = 0
+overlapCounter = 0
+for line in data.readlines():
+    line = line.strip('\n')
+    firstElf, secondElf = line.split(',')
+    firstElf = firstElf.split('-')
+    secondElf = secondElf.split('-')
+    # PART 1
+    # if int(firstElf[0]) <= int(secondElf[0]) and int(firstElf[1]) >= int(secondElf[1]):
+    #     fullyContainCounter += 1
+    #
+    # elif int(secondElf[0]) <= int(firstElf[0]) and int(secondElf[1]) >= int(firstElf[1]):
+    #     fullyContainCounter += 1
+
+    # PART 2
+    if int(firstElf[0]) <= int(secondElf[0]) <= int(firstElf[1]):
+        overlapCounter += 1
+    elif int(firstElf[0]) <= int(secondElf[1]) <= int(firstElf[1]):
+        overlapCounter += 1
+    elif int(secondElf[0]) <= int(firstElf[0]) <= int(secondElf[1]):
+        overlapCounter += 1
+    elif int(secondElf[0]) <= int(firstElf[1]) <= int(secondElf[1]):
+        overlapCounter += 1
+print(fullyContainCounter)
+print(overlapCounter)
