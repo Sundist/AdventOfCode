58c4d4660297accf02bf3d6c776e379afded787a
Sundist <sundisttt@gmail.com>
2022-12-06 11:49:11 +0300

    AOC 2022

--- AOC2022/Day_06/day6.py
+++ AOC2022/Day_06/day6.py
@@ -2,13 +2,13 @@ with open('input.txt', 'r') as data:
     data_stream = data.read().strip('\n')
 
 ## PART 1 ##
-# i = 0
-# marker = ''  # 4 chars
-# while len(set(marker)) != 4 and i < len(data_stream):
-#     marker = ''.join(data_stream[i:i + 4])
-#     if len(set(marker)) == 4:
-#         print(marker, data_stream[i:i + 4], set(marker), i, i + 4)
-#     i += 1
+i = 0
+marker = ''  # 4 chars
+while len(set(marker)) != 4 and i < len(data_stream):
+    marker = ''.join(data_stream[i:i + 4])
+    if len(set(marker)) == 4:
+        print(marker, data_stream[i:i + 4], set(marker), i, i + 4)
+    i += 1
 
 ## PART 2 ##
 i = 0
